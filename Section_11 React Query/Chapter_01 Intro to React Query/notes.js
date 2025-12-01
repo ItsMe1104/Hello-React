e622f3e57bc62d18450c565ba7ab5be6be4a1459
Hrithik <hey.its.me.hrithik@gmail.com>
2025-12-02 04:37:11 +0530

    Add files for Chapter 01 of Section 11 :- 'React Query'

---
+++ Section_11 React Query/Chapter_01 Intro to React Query/notes.js
@@ -0,0 +1,19 @@
+//?? 1) React Query
+
+// --> It is the best library for managing, fetching and caching data from API requests
+// --> It also helps to manage the cached data for our API requests
+// --> It increases the app's performance
+
+
+//?? What is cache :-
+// --> It is like temporary storage area in memory where fetched data is kept
+// --> When we make the same request again, it can serve the data from the cache directly instead of fetching again
+
+
+//?? Features of React Query :-
+// --> Caching our data
+// --> Automatic retries if server doesn't respond
+// --> Request cancellation when a component unmounts
+// --> Automatically get the updated data from the API at set intervals (every 2 mins)
+
+
