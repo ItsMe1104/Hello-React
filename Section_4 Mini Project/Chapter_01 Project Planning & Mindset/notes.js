a2d765ac8ee065f5e6a510e39336a077f3b7f912
Hrithik <hey.its.me.hrithik@gmail.com>
2025-12-05 02:22:49 +0530

    Update files for chapter 01 of Section 04 :- 'Mini Project'

--- Section_4 Mini Project/Chapter_01 Project Planning & Mindset/notes.js
+++ Section_4 Mini Project/Chapter_01 Project Planning & Mindset/notes.js
@@ -1,1 +1,19 @@
 //?? 1) Planning & Mindset for developing the Application
+
+//?? Project Planning :-
+// i) Requirements (coming from Product Owner)
+
+// ii) Design :
+// --> Creating HTML pages
+// --> Designing them using Layouts and Figma design
+// --> Implementing the Design and then the functionality
+
+
+//?? NOTE :-
+//* a) Earlier we use to have waterfall model for software development
+// Requirements --> Design --> Development --> Testing --> Deployment --> Maintenance
+
+//* b) Now we use the agile methodologies :-
+// --> It is the base of sprint planning
+// --> Here the client can give immediate feedback
+// --> The changes will reflect in the dev, sit, etc environments
