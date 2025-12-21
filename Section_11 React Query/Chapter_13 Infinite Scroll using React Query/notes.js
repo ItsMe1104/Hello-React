9d56a69df8886084c4f570809c086db27dfbd35a
ItsMe1104 <your-github-email@example.com>
2025-12-22 01:15:08 +0530

    Add files for Chapter 13 of Section 11 :- "React Query"

---
+++ Section_11 React Query/Chapter_13 Infinite Scroll using React Query/notes.js
@@ -0,0 +1,14 @@
+//?? 1) What is Infinite Scroll?
+
+// --> It is a UI/UX pattern where more data is automatically loaded & appended as the user scrolls down
+// --> It is an alternate of Pagination
+// --> It is mainly used for addictive websites where we want the user to keep getting engaged (e.g Instagram)
+
+
+//? How it works (step by step)
+// --> Initial data is loaded (e.g., first 10 items)
+// --> User scrolls down
+// --> App detects user is near the bottom
+// --> App fetches the next set of items
+// --> New items are appended, not replaced
+// --> Repeat until no more data
